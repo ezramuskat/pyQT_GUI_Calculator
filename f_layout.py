cda84f45a0115cab1b8536dfcae744352ab2599f
Ezra Muskat <razeprime1@gmail.com>
2019-12-31 17:26:34 +0200

    QForm variant

---
+++ f_layout.py
@@ -0,0 +1,19 @@
+# Filename: g_layout.py
+
+import sys
+
+from PyQt5.QtWidgets import QApplication, QFormLayout, QWidget, QLineEdit
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle('QFormLayout')
+layout = QFormLayout()
+layout.addRow('Name: ', QLineEdit())
+layout.addRow('Age: ', QLineEdit())
+layout.addRow('Job: ', QLineEdit())
+layout.addRow('Hobbies: ', QLineEdit())
+window.setLayout(layout)
+window.show()
+
+sys.exit(app.exec_())
